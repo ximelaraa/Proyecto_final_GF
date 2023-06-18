b6ec8f2e006a44504a446c41a908996111c32236
Beck26 <120044122+Beck26@users.noreply.github.com>
2023-06-18 15:56:52 -0600

    Update Script_1.R

--- PF_GF/Script_1.R
+++ PF_GF/Script_1.R
@@ -1,4 +1,6 @@
 ## De .csv a phyloseq
+TAXA <- read_delim("PF_GF/domain;phylum;class;order;family;ge.txt", 
+                   +     delim = ";", escape_double = FALSE, trim_ws = TRUE)
 o<-sequence(3759)
 t<-rep("otu", 3759) 
 otu<-paste(t,o, sep = "_")
@@ -6,3 +8,4 @@ otu
 taxa<-cbind(TAXA,otu) #TAXA es la tabla que envié antes solo que le pusi ese nombre
 
 ##hola esto es una prueba
+otu_mat <- as.matrix(taxa)
